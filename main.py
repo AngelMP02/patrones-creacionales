31b27bc4db2ea594bb9e016e438c9e61c1fa8c7d
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-08 16:29:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ data = pd.read_csv(URL, sep=';', encoding='ISO-8859-1')
  
 
 print(data.head())  # Mostrar las primeras filas para visualizar los datos
+
+data = data.drop(columns=['columna_a_eliminar'])
+
+# 2. Tratar valores faltantes (rellena los valores faltantes con un valor específico, por ejemplo, 0)
+data = data.fillna(0)
+
+# 3. Cambiar tipos de datos (sustituye 'nombre_columna' por el nombre de la columna y 'nuevo_tipo' por el tipo de datos deseado)
+data['nombre_columna'] = data['nombre_columna'].astype('nuevo_tipo')
+
+# 4. Renombrar columnas (sustituye 'nombre_antiguo' por el nombre antiguo y 'nombre_nuevo' por el nuevo nombre)
+data = data.rename(columns={'nombre_antiguo': 'nombre_nuevo'})
+
+# 5. Eliminar duplicados
+data = data.drop_duplicates()
+
+# Mostrar las primeras filas para visualizar los datos limpios
+print(data.head())
+
+
+
