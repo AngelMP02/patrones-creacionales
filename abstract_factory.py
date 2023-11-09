c6f9386a81536d760878025e26b17040b8de4938
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-09 09:08:56 +0100

    Update abstract_factory.py

--- abstract_factory.py
+++ abstract_factory.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+import pandas as pd
 
 
 class AbstractFactory(ABC):
@@ -53,13 +54,8 @@ class ConcreteFactory_HoraIntervencion(AbstractFactory):
 
 
 class AbstractModa(ABC):
-    """
-    Each distinct product of a product family should have a base interface. All
-    variants of the product must implement this interface.
-    """
-
     @abstractmethod
-    def useful_function_a(self) -> str:
+    def calcular(self):
         pass
 
 
@@ -69,14 +65,20 @@ Concrete Products are created by corresponding Concrete Factories.
 
 
 class ConcreteModa_HoraSolicitud(AbstractModa):
+    def __init__(self, datos):
+            self.datos = datos
     def useful_function_a(self) -> str:
         return "The result of the product A1."
 
 
 class ConcreteModa_Mes(AbstractModa):
+    def __init__(self, datos):
+            self.datos = datos
     def useful_function_a(self) -> str:
         return "The result of the product A2."
 class ConcreteModa_HoraIntervencion(AbstractModa):
+    def __init__(self, datos):
+            self.datos = datos
     def useful_function_a(self) -> str:
         return "The result of the product A2."
 
@@ -111,7 +113,7 @@ Concrete Products are created by corresponding Concrete Factories.
 
 
 class ConcreteMedia_HoraSolicitud(AbstractMedia):
-     def __init__(self, datos):
+    def __init__(self, datos):
             self.datos = datos
 
     def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
@@ -168,13 +170,43 @@ def client_code_media(factory: AbstractFactory) -> None:
 
 
 if __name__ == "__main__":
-    """
-    The client code can work with any concrete factory class.
-    """
-    print("Client: Testing client code with the first factory type:")
-    client_code(ConcreteFactory1())
+    
+data = pd.read_csv('Ejercicio1/activaciones_samur_2022.csv', delimiter = ";")
 
-    print("\n")
+print(data.info()) 
+print(data.describe())  
+print(data.head()) 
 
-    print("Client: Testing the same client code with the second factory type:")
-    client_code(ConcreteFactory2())
+#elimino los valores nulos para que no me de error al procesar lso datos
+data = data.dropna()
+
+#compruebo que no hay valores nulos en el dataset
+nulos = data.isna().sum()
+print(nulos)
+print(data.columns)
+meses_a_numeros = {
+    'ENERO': 1,
+    'FEBRERO': 2,
+    'MARZO': 3,
+    'ABRIL': 4,
+    'MAYO': 5,
+    'JUNIO': 6,
+    'JULIO': 7,
+    'AGOSTO': 8,
+    'SEPTIEMBRE': 9,
+    'OCTUBRE': 10,
+    'NOVIEMBRE': 11,
+    'DICIEMBRE': 12
+}
+
+data['Mes'] = data['Mes'].map(meses_a_numeros)
+
+data['Hora Solicitud'] = pd.to_datetime(data['Hora Solicitud'], format='%H:%M:%S')
+data['Hora Solicitud'] = (data['Hora Solicitud'].dt.hour * 60 + data['Hora Solicitud'].dt.minute + data['Hora Solicitud'].dt.second / 60).round().astype('int64')
+
+data['Hora Intervención'] = pd.to_datetime(data['Hora Intervención'], format='%H:%M:%S')
+data['Hora Intervención'] = (data['Hora Intervención'].dt.hour * 60 + data['Hora Intervención'].dt.minute + data['Hora Intervención'].dt.second / 60).round().astype('int64')
+
+data.drop('Año', axis=1, inplace=True)
+
+data_limpio = data.copy()
