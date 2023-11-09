fad3c1a7e872f7032236fcf8e1d10e737ba9dd06
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-09 20:53:42 +0100

    cambios

---
+++ csv_writer.py
@@ -0,0 +1,17 @@
+import csv
+
+class PizzaCSVWriter:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def write_pizza_to_csv(self, pizza):
+        with open(self.file_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([
+                pizza.tipo_masa,
+                pizza.salsa,
+                ', '.join(pizza.ingredientes_principales),
+                pizza.tecnicas_coccion,
+                pizza.presentacion,
+                pizza.maridaje_recomendado
+            ])
