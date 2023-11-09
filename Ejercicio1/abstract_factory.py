2a919c71c63c1262877d79a79e1f4604c368df24
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-09 10:40:37 +0100

    Update abstract_factory.py

--- Ejercicio1/abstract_factory.py
+++ Ejercicio1/abstract_factory.py
@@ -22,13 +22,11 @@ class AbstractFactory(ABC):
 
 
 class ConcreteFactory_HoraSolicitud(AbstractFactory):
-   
-        
 
-    def create_product_a(self) -> AbstractModa:
+    def moda(self) -> AbstractModa:
         return ConcreteModa_HoraSolicitud(self.datos)
 
-    def create_product_b(self) -> AbstractMedia:
+    def media(self) -> AbstractMedia:
         return ConcreteMedia_HoraSolicitud(self.datos)
 
 
@@ -37,19 +35,19 @@ class ConcreteFactory_Mes(AbstractFactory):
     Each Concrete Factory has a corresponding product variant.
     """
 
-    def create_product_a(self) -> AbstractModa:
+    def moda(self) -> AbstractModa:
         return ConcreteModa_Mes(self.datos)
 
-    def create_product_b(self) -> AbstractMedia:
+    def media(self) -> AbstractMedia:
         return ConcreteMedia_Mes(self.datos)
 
 class ConcreteFactory_HoraIntervencion(AbstractFactory):
   
 
-    def create_product_a(self) -> AbstractModa:
+    def moda(self) -> AbstractModa:
         return ConcreteModa_HoraIntervencion(self.datos)
 
-    def create_product_b(self) -> AbstractMedia:
+    def media(self) -> AbstractMedia:
         return ConcreteMedia_HoraIntervencion(self.datos)
 
 
@@ -137,42 +135,50 @@ def client_code_media(factory: AbstractFactory) -> None:
 
 if __name__ == "__main__":
     
-data=pd.read_csv('Ejercicio1/activaciones_samur_2022.csv', delimiter = ";")
+    data=pd.read_csv('Ejercicio1/activaciones_samur_2022.csv', delimiter = ";")
+    #elimino los valores nulos para que no me de error al procesar lso datos
+    data = data.dropna()
 
-print(data.info()) 
-print(data.describe())  
-print(data.head()) 
+    #compruebo que no hay valores nulos en el dataset
+    nulos = data.isna().sum()
+    print(nulos)
+    print(data.columns)
+    meses_a_numeros = {
+        'ENERO': 1,
+        'FEBRERO': 2,
+        'MARZO': 3,
+        'ABRIL': 4,
+        'MAYO': 5,
+        'JUNIO': 6,
+        'JULIO': 7,
+        'AGOSTO': 8,
+        'SEPTIEMBRE': 9,
+        'OCTUBRE': 10,
+        'NOVIEMBRE': 11,
+        'DICIEMBRE': 12
+    }
 
-#elimino los valores nulos para que no me de error al procesar lso datos
-data = data.dropna()
+    data['Mes'] = data['Mes'].map(meses_a_numeros)
 
-#compruebo que no hay valores nulos en el dataset
-nulos = data.isna().sum()
-print(nulos)
-print(data.columns)
-meses_a_numeros = {
-    'ENERO': 1,
-    'FEBRERO': 2,
-    'MARZO': 3,
-    'ABRIL': 4,
-    'MAYO': 5,
-    'JUNIO': 6,
-    'JULIO': 7,
-    'AGOSTO': 8,
-    'SEPTIEMBRE': 9,
-    'OCTUBRE': 10,
-    'NOVIEMBRE': 11,
-    'DICIEMBRE': 12
-}
+    data['Hora Solicitud'] = pd.to_datetime(data['Hora Solicitud'], format='%H:%M:%S')
+    data['Hora Solicitud'] = (data['Hora Solicitud'].dt.hour * 60 + data['Hora Solicitud'].dt.minute + data['Hora Solicitud'].dt.second / 60).round().astype('int64')
 
-data['Mes'] = data['Mes'].map(meses_a_numeros)
+    data['Hora Intervención'] = pd.to_datetime(data['Hora Intervención'], format='%H:%M:%S')
+    data['Hora Intervención'] = (data['Hora Intervención'].dt.hour * 60 + data['Hora Intervención'].dt.minute + data['Hora Intervención'].dt.second / 60).round().astype('int64')
 
-data['Hora Solicitud'] = pd.to_datetime(data['Hora Solicitud'], format='%H:%M:%S')
-data['Hora Solicitud'] = (data['Hora Solicitud'].dt.hour * 60 + data['Hora Solicitud'].dt.minute + data['Hora Solicitud'].dt.second / 60).round().astype('int64')
-
-data['Hora Intervención'] = pd.to_datetime(data['Hora Intervención'], format='%H:%M:%S')
-data['Hora Intervención'] = (data['Hora Intervención'].dt.hour * 60 + data['Hora Intervención'].dt.minute + data['Hora Intervención'].dt.second / 60).round().astype('int64')
-
-data.drop('Año', axis=1, inplace=True)
-
-data_limpio = data.copy()
+    data.drop('Año', axis=1, inplace=True)
+    factory_hora_solicitud = ConcreteFactory_HoraSolicitud(data)
+    factory_hora_intervencion = ConcreteFactory_HoraIntervencion(data)
+    factory_mes = ConcreteFactory_Mes(data)
+    
+    
+    print("Hora de Solicitud:")
+    client_code_mode(factory_hora_solicitud)
+    client_code_media(factory_hora_solicitud)
+    print("Mes:")
+    client_code_mode(factory_mes)
+    client_code_media(factory_mes)
+    print("Hora Intervencion:")
+    client_code_mode(factory_hora_intervencion)
+    client_code_media(factory_hora_intervencion)
+   
