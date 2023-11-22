46e1e5339c8762a5e4d2f1f32bce6e98042a16fe
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-22 11:53:29 +0100

    Update pizza.py

--- Ejercicio2/pizza.py
+++ Ejercicio2/pizza.py
@@ -1,4 +1,5 @@
 #Define la clase Pizza que representa la estructura básica de una pizza con sus propiedades.
+# pizza.py
 
 class Pizza:
     def __init__(self):
@@ -8,3 +9,4 @@ class Pizza:
         self.tecnicas_coccion = ""
         self.presentacion = ""
         self.maridaje_recomendado = ""
+        self.bebida = ""  # Agregar el atributo de bebida a la clase Pizza
